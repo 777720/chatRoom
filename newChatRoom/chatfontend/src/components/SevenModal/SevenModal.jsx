e82d90d0434523a1899bd07cdd1cc7e979911471
daFuShuai <627078086@qq.com>
2019-01-16 12:57:44 +0800

    modal

--- newChatRoom/chatfontend/src/components/SevenModal/SevenModal.jsx
+++ newChatRoom/chatfontend/src/components/SevenModal/SevenModal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 import PortalWrapper from './PortalWrapper'
+import SevenTransition from './SevenTransition'
 import './SevenModal.css'
   
   
@@ -19,82 +20,30 @@ class SevenModal extends React.Component {
   componentWillReceiveProps(props) {
 
     if(props.visible) {
-      this.enterAnimate()
+      this.setState({
+        visible: true
+      })
     }
   }
 
-  enterAnimate = () => {
-    const enterClasses = "sevenmodal-enter"
-    const enterActiverClasses = 'sevenmodal-enter-active'
-    const enterEndClasses = 'sevenmodal-enter-end'
-
-    const enterMaskClasses = "sevenmodal-mask-enter"
-    const enterActiveClasses = "sevenmodal-mask-active"
-
-    const enterTimeout = 50
-    const enterActiveTimeout = 200
-
-    this.setState({
-      visible: true, 
-      classes: enterClasses,
-      maskClasses: enterMaskClasses,
-    })
-
-    const enterActiverTimer = setTimeout(() => {
-      this.setState({ 
-        classes: enterActiverClasses,
-        maskClasses: enterActiveClasses 
-      }, clearTimeout(enterActiverTimer))
-    }, enterTimeout)
-
-
-    const initTimer = setTimeout(() => {
-      this.setState({ classes: '', maskClasses: '' }, clearTimeout(initTimer))
-    }, enterTimeout + enterActiveTimeout)
-  }
-
-  leaveAnimate = () => {
-    const leaveActiverClasses = 'sevenmodal-leave-active'
-    const leaveEndClasses = 'sevenmodal-leave-end'
-
-    const leaveMaskClasses = "sevenmodal-mask-leave"
-    const leaveMaskActiveClasses = "sevenmodal-mask-leaveactive"
-
-    const leaveTimeout = 0
-    const leaveActiveTimeout = 100
-    const leaveEndTimeout = 200
-
-    this.setState({
-      classes: leaveActiverClasses,
-      maskClasses: leaveMaskClasses
-    })
-
-    const leaveActiveTimer = setTimeout(() => {
-      this.setState({
-        classes: leaveEndClasses,
-      }, clearTimeout(leaveActiveTimer))
-    }, leaveTimeout + leaveActiveTimeout)
-
-    const initTimer = setTimeout(() => {
-      this.setState({
-        classes: '',
-        visible: false
-      }, clearTimeout(initTimer))
-    }, leaveTimeout + leaveActiveTimeout + 10)
-
-
-  }
+  
 
   closeModal = () => {
     const { onClose } = this.props
     onClose && onClose()
-    this.leaveAnimate()
+    this.setState({
+      visible: false
+    })
   }
 
   confirmModal = () => {
     const { onConfirm } = this.props
     onConfirm && onConfirm()
-    this.leaveAnimate()
+
+    this.setState({
+      visible: false
+    })
+    // this.leaveAnimate()
   }
 
 
@@ -104,27 +53,23 @@ class SevenModal extends React.Component {
 
 
   render() {
-    const { visible, classes, maskClasses } = this.state
-    console.log('classes', classes, maskClasses);
+    const { visible, classes } = this.state
     return (
       <PortalWrapper>
-        <div className="sevenmodal-wrapper">
-          {
-            visible &&
-            <div>
-              <div className={`sevenmodal ${classes}`}>
-                <div className="sevenmodal-title">这是modal标题</div>
-                <div className="sevenmodal-content">这是modal内容</div>
-                <div className="sevenmodal-operator">
-                  <button className="sevenmodal-operator-close" onClick={this.closeModal}>取消</button>
-                  <button className="sevenmodal-operator-confirm" onClick={this.confirmModal}>确认</button>
-                </div>
-
-              </div>
-              <div className={`sevenmodal-mask ${maskClasses}`} onClick={this.maskClick}></div> 
+        <SevenTransition
+          visible={visible}
+        >
+          <div className={`sevenmodal`}>
+            <div className="sevenmodal-title">这是modal标题</div>
+            <div className="sevenmodal-content">这是modal内容</div>
+            <div className="sevenmodal-operator">
+              <button className="sevenmodal-operator-close" onClick={this.closeModal}>取消</button>
+              <button className="sevenmodal-operator-confirm" onClick={this.confirmModal}>确认</button>
             </div>
-          }
-        </div> 
+
+          </div>
+          <div className={`sevenmodal-mask`} onClick={this.maskClick}></div>
+        </SevenTransition>
       </PortalWrapper> 
     )
   }
---
+++ newChatRoom/chatfontend/src/components/SevenModal/SevenTransition.jsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+  
+  
+class SevenTransition extends React.Component {
+  state = {
+    modalClasses: '',
+    maskClasses: 'test',
+    currrentVisable: false,
+  } 
+
+
+
+  componentWillReceiveProps(nextProps) {
+    const { animation } = nextProps
+    if (!animation) {
+      return
+    }
+    if (!nextProps.visible) {
+      console.log('ebter leaveAnimation');
+      this.leaveAnimation(nextProps.visible)
+    } else {
+      console.log('enter enterAnimation');
+      this.enterAnimation(nextProps.visible)
+    }
+  }
+  
+
+  enterAnimation(status) {
+    const enterClass = 'sevenmodal-enter '
+    const enterActiveClass = 'sevenmodal-enter-active'
+    const enterEndClass = 'sevenmodal-enter-end'
+
+    const enterMaskClass = 'sevenmodal-mask-enter'
+    const activeMaskClass = 'sevenmodal-mask-active'
+
+    const enterClassTime = 100
+    const enterActiveTime = 200
+
+    this.setState({
+      currrentVisable: status,
+      modalClasses: enterClass,
+      maskClasses: enterMaskClass
+    })
+
+    const enterActiveTimeout = setTimeout(() => {
+      this.setState({
+        modalClasses: enterActiveClass,
+        maskClasses: activeMaskClass
+      })
+      clearTimeout(enterActiveTimeout)
+    }, enterClassTime)
+
+    const endActiveTimeout = setTimeout(() => {
+      this.setState({
+        modalClasses: enterEndClass
+      })
+      clearTimeout(endActiveTimeout)
+    }, enterActiveTime + enterActiveTime)
+    const initTimeout = setTimeout(() => {
+      this.setState({
+        modalClasses: '',
+        maskClasses: ''
+      })
+      clearTimeout(initTimeout)
+    }, enterActiveTime + enterActiveTime + 10)
+  }
+
+  leaveAnimation(status) {
+    const leaveClass = 'sevenmodal-leave'
+    const leaveActiveClass = 'sevenmodal-leave-active'
+    const leaveEndClass = 'sevenmodal-leave-end'
+
+    const leaveMaskClass = 'sevenmodal-mask-leave'
+    const leaveActiveMaskClass = 'sevenmodal-mask-leaveactive'
+
+    const leaveTime = 10
+    const leaveActiveTime = 200
+    const leaveEndTime = 100
+
+    this.setState({
+      modalClasses: leaveClass,
+    })
+
+    const leaveActiveTimeout = setTimeout(() => {
+      this.setState({
+        modalClasses: leaveActiveClass,
+        maskClasses: leaveMaskClass
+      })
+      clearTimeout(leaveActiveTimeout)
+    }, leaveTime)
+
+    const leaveEndActiveTimeout = setTimeout(() => {
+      this.setState({
+        modalClasses: leaveEndClass,
+        maskClasses: leaveActiveMaskClass
+      })
+    }, leaveTime + leaveActiveTime)
+
+    const initTimeout = setTimeout(() => {
+      this.setState({
+        modalClasses: '',
+        currrentVisable: status,
+        maskClasses: ''
+      })
+      clearTimeout(initTimeout)
+    }, leaveTime + leaveActiveTime + leaveEndTime)
+
+  }
+
+  cloneChildren = () => {
+    const { children } = this.props
+    const { modalClasses, maskClasses } = this.state
+    const eleArr = children.map((item, index) => {
+      const oldClassname = item.props.className
+      if(index === 0) {
+        return (
+          React.cloneElement(
+            item,
+            { className: `${oldClassname} ${modalClasses}` }
+          )
+        )
+      } else if (index === 1) {
+        return (
+          React.cloneElement(
+            item,
+            { className: `${oldClassname} ${maskClasses}` }
+          )
+        )
+      } else {
+        return (
+          React.cloneElement(
+            item,
+          )
+        )
+      }
+    })
+    return eleArr
+  }
+
+  render() {
+    const { currrentVisable } = this.state
+    return currrentVisable && this.cloneChildren()
+    
+  }
+}
+  
+  
+SevenTransition.defaultProps = {
+  animation: true,
+}
+SevenTransition.propTypes = {
+  animation: PropTypes.bool,
+  visible: PropTypes.bool,
+}
+  
+  
+export default SevenTransition

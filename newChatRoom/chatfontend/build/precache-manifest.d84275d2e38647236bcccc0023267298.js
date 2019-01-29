935f030be21841933f367bf1a98dcc72002ab1c4
daFuShuai <627078086@qq.com>
2019-01-29 16:46:10 +0800

    --prod_dispatch--

---
+++ newChatRoom/chatfontend/build/precache-manifest.d84275d2e38647236bcccc0023267298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d1b750353f503336c8e",
+    "url": "/static/js/main.1d1b7503.chunk.js"
+  },
+  {
+    "revision": "0f5991ac5367e244b01c",
+    "url": "/static/js/1.0f5991ac.chunk.js"
+  },
+  {
+    "revision": "1d1b750353f503336c8e",
+    "url": "/static/css/main.ed0ee345.chunk.css"
+  },
+  {
+    "revision": "581020cddc84864a505876b9cf63b900",
+    "url": "/index.html"
+  }
+];
